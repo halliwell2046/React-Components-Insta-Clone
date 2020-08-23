1055b3e60118153f3a9dceaae6b34439a82a973b
halliwell2046 <halliwell2046@yahoo.com>
2020-08-23 16:35:58 -0400

    images but getting error

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,13 +2,14 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes } = props;
+  const { userLiked, setUserLiked } = useState(false);
 
   return (
     <div>
@@ -16,16 +17,23 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div className='like-section-wrapper' onClick={like}>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
-    </div>
+      <p className='like-number'>100 likes{numberOfLikes} likes </p>
+      </div>
   );
+
+  function like() {
+    if (!userLiked) {
+      likePost();
+      setUserLiked(true);
+    };
+  };
 };
 
 export default LikeSection;
